8c5a7c0caf0b5f89e85c8ba71f9a14e5087b74cf
Govind Rai <raigovind93@gmail.com>
2017-06-26 15:28:20 -0700

    Add infolets and complete Task 1 & 2

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -5,11 +5,74 @@
 /*
  * Your dashboard ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery'],
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojmasonrylayout', 'ojs/ojchart', 'ojs/ojgauge'],
  function(oj, ko, $) {
-  
+
     function DashboardViewModel() {
       var self = this;
+
+      self.barChart = {
+          name: 'Something else vs. Won',
+          sizeClass: 'oj-masonrylayout-tile-3x2'
+        };
+
+              /* toggle button variables */
+        self.orientationValue = ko.observable('vertical');
+        self.renderSeparators = ko.observable("on");
+
+        self.separatorColor = ko.observable('#C4CED7');
+        self.dataSetValue = ko.observable('even');
+
+        /* even hierarchy chart data example */
+        var seriesItems1 = [];
+        var seriesItems2 = [];
+        for (var g = 0; g < 5; g++) {
+           var randomValue1 = (1000 + (Math.random() * 1000)) * 750;
+           seriesItems1.push(randomValue1);
+           var randomValue2 = (1000 + (Math.random() * 1000)) * 500;
+           seriesItems2.push(randomValue2);
+        }
+        var seriesValue = [{name: "Quota", items: seriesItems1},
+                           {name: "Won", items: seriesItems2}];
+
+
+        self.seriesValue = ko.observableArray(seriesValue);
+
+        var converterFactory = oj.Validation.converterFactory('number');
+        var currencyConverter = converterFactory.createConverter({style: 'currency', currency: 'USD'});
+        self.yAxisConverter = ko.observable(currencyConverter);
+
+        var valueFormat = [{type:'y', converter: currencyConverter}, {type: 'group', tooltipLabel: ['Region', 'Department', 'Quarter']}];
+        self.valueFormatsValue = ko.observable(valueFormat);
+
+        var styleData = {groupSeparators: {rendered:ko.toJS(self.renderSeparators), color: ko.toJS(self.separatorColor)}};
+        self.styleDefaultValue = ko.observable(styleData);
+
+      self.teamLeads = {
+        name: 'Team Leads',
+        sizeClass: 'oj-masonrylayout-tile-1x1'
+
+      };
+      self.orders = {
+        name: 'Orders',
+        sizeClass: 'oj-masonrylayout-tile-1x1'
+      };
+      self.serviceRequests = {
+        name: 'Service Requests',
+        sizeClass: 'oj-masonrylayout-tile-1x1'
+      };
+      self.myVacation = {
+        name: 'My Vacation',
+        sizeClass: 'oj-masonrylayout-tile-1x1'
+      };
+
+
+
+
+
+
+
+
       // Below are a subset of the ViewModel methods invoked by the ojModule binding
       // Please reference the ojModule jsDoc for additionaly available methods.
 
@@ -43,7 +106,7 @@ define(['ojs/ojcore', 'knockout', 'jquery'],
 
 
       /**
-       * Optional ViewModel method invoked after the bindings are applied on this View. 
+       * Optional ViewModel method invoked after the bindings are applied on this View.
        * If the current View is retrieved from cache, the bindings will not be re-applied
        * and this callback will not be invoked.
        * @param {Object} info - An object with the following key-value pairs:
